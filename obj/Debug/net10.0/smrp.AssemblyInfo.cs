02bdedf078848122741da1bc1c77a1c2f7a2ee1c
wfsiew-nova <siewwingfei@hotmail.com>
2026-02-08 22:26:05 +0800

    u

--- obj/Debug/net10.0/smrp.AssemblyInfo.cs
+++ obj/Debug/net10.0/smrp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("smrp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5851dae8799b660168cabbdfbebef89e7b6f1073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cfe9fb5acdca89f0a3956cae66e28b29ab84d7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("smrp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("smrp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
