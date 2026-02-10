adf523bf533a90419d62f40fd729dbfdd4787a01
wfsiew-nova <siewwingfei@hotmail.com>
2026-02-11 00:30:21 +0800

    u

--- obj/Debug/net10.0/smrp.AssemblyInfo.cs
+++ obj/Debug/net10.0/smrp.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731200970b47c313242945347ee754cc7ad639a8")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef27591cd3256e56365e57f18df42470ca3ab876")]
+>>>>>>> Stashed changes
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef27591cd3256e56365e57f18df42470ca3ab876")]
 >>>>>>> Stashed changes
